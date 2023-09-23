9dc2dae9c67b0b1aa9dbfdd2c7489365e2e00143
Minhnq24751 <mn9175719@gmail.com>
2023-09-23 23:22:49 +0700

    homepage and menpage

--- FE/src/router.tsx
+++ FE/src/router.tsx
@@ -2,10 +2,12 @@
 import {createBrowserRouter} from "react-router-dom"
 import LayoutWebsite from "./components/pages/layout/layoutWebsites"
 import HomePage from "./components/pages/home/homePage"
+import Men from "./components/pages/home/menPage"
 
 export const router = createBrowserRouter([
     {path: "/", element: <LayoutWebsite/>,children:[
-        {index: true, element:<HomePage/>}
+        {index: true, element:<HomePage/>},
+        {path: "menPage", element:<Men/>}
     ]},
    
 ])
