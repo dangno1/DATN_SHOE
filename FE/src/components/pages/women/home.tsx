b7431f68268207554b39903965bd35a313ff175c
Dang <97234801+dangno1@users.noreply.github.com>
2023-09-27 22:29:00 +0700

    update

--- FE/src/components/pages/women/home.tsx
+++ FE/src/components/pages/women/home.tsx
@@ -1,19 +1,15 @@
-import Header from "./header";
 import Slider from "./banner";
-import Footer from "../home/footer";
 import WomenList from "./womenList";
 import Social from "./social";
 
 const Women = () => {
-    return (
-      <div>
-          <Header/>
-          <Slider/>
-          <Social/>
-         <WomenList/>
-          <Footer/>
-      </div>
-    )
-  }
-  
-  export default Women
+  return (
+    <div>
+      <Slider />
+      <Social />
+      <WomenList />
+    </div>
+  );
+};
+
+export default Women;
