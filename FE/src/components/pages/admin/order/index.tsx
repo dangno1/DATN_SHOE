d01ba1ae4886039164df9ff7ade04146b753c586
quangvann <ngovanquang103@gmail.com>
2023-10-11 01:30:59 +0700

    update-order

--- FE/src/components/pages/admin/order/index.tsx
+++ FE/src/components/pages/admin/order/index.tsx
@@ -1,12 +1,10 @@
 
 
-import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai'
-
 const index = () => {
     return (
         <div>
             <div className="py-3 px-4 flex items-center text-sm font-medium leading-none text-gray-600 bg-gray-200 hover:bg-gray-300 cursor-pointer rounded" >
-                <p>Category</p>
+                <p>Trạng Thái</p>
                 <select aria-label="select" className="focus:text-indigo-600 focus:outline-none bg-transparent ml-1">
                     <option className="text-sm text-indigo-800">Delivering</option>
                     <option className="text-sm text-indigo-800">Returns</option>
@@ -25,7 +23,7 @@ const index = () => {
                                                 <div className="flex items-center gap-x-3">
                                                     <input type="checkbox" className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
                                                     <button className="flex items-center gap-x-2">
-                                                        <span>PRODUCT</span>
+                                                        <span>STT</span>
 
                                                         <svg className="h-3" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                             <path d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z" fill="currentColor" stroke="currentColor" stroke-width="0.1" />
@@ -36,24 +34,28 @@ const index = () => {
                                                 </div>
                                             </th>
 
+
                                             <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                            BUYING TIME
+                                                MÃ ĐƠN
+                                            </th>
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                THỜI GIAN
                                             </th>
 
                                             <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                            INTO MONEY
+                                                TỔNG PHÍ DỊCH VỤ
                                             </th>
 
                                             <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                              USER
+                                                BÊN NHẬN
                                             </th>
 
                                             <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                            NOTE
+                                                GHI CHÚ ĐƠN HÀNG
                                             </th>
 
                                             <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                            STATUS
+                                                TRẠNG THÁI
                                             </th>
 
                                             <th scope="col" className="relative py-3.5 px-4">
@@ -63,17 +65,14 @@ const index = () => {
                                     </thead>
                                     <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                                         <tr>
-                                            <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
+                                            <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap" >
                                                 <div className="inline-flex items-center gap-x-3">
                                                     <input type="checkbox" className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
-                                                    <div>
-                                                        <img src="https://bizweb.dktcdn.net/100/410/787/products/m2nltcj4uwd-mocbytimil-ldcrt-ac7txspjkhscowikr-vn5yme81isbw3cowmraele-pksvhgjoengixy-ynulerltdon11-ynjvd9s0lidajytp99nfejifcadfw68oj8s9cdbzm3urk3lgyplum2jyjpop213-runk7s-d1eur4ex03d9xocrontldqcpimsmjqc2v0iyvdd6fnxnyx2eqox.jpg?v=1668486912753" alt="" width={200} height={200} />
-                                                        <p className="leading-relaxed">Giày Adidas</p>
-                                                        <p className="leading-relaxed">Quantity: <span className="text-red-500">2</span></p>
-
-                                                    </div>
-
+                                                    <p className="leading-relaxed text-red-500"> 1</p>
                                                 </div>
+                                            </td>
+                                            <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
+                                            <p className="leading-relaxed text-red-500">5FHLA$UL</p>
                                             </td>
                                             <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Jan 6, 2022</td>
                                             <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
@@ -88,120 +87,14 @@ const index = () => {
                                             <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
                                                 <div className="flex items-center gap-x-2">
                                                     <img className="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
-                                                    
+
                                                     <div>
                                                         <h2 className="text-sm font-medium text-gray-800 dark:text-white ">Quang</h2>
                                                         <p className="text-xs font-normal text-gray-600 dark:text-gray-400">quang@.com</p>
                                                     </div>
                                                 </div>
                                             </td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">đơn hàng đã giao thành công</td>
-                                            <td className='px-4 py-4 text-sm whitespace-nowrap'>
-                                                < select id="cars" className="w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-300">
-                                                <option value="" className="py-2">waiting for delivery</option>
-                                                    <option value="" className="py-2">are delivering</option>
-                                                    <option value="" className="py-2">Successful delivery</option>
-                                                    <option value="" className="py-2">Returns</option>
-                                                </select>
-                                            </td>
-                                            <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                                <div className="flex items-center gap-x-6">
-                                                    <button className="text-gray-500 transition-colors duration-200 dark:hover:text-indigo-500 dark:text-gray-500 hover:text-indigo-500 focus:outline-none">
-                                                        <AiOutlineDelete />
-                                                    </button>
-
-                                                    <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
-                                                        <AiOutlineEdit />
-                                                    </button>
-                                                </div>
-                                            </td>
-                                        </tr>
-
-
-                                        <tr>
-                                            <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                                                <div className="inline-flex items-center gap-x-3">
-                                                    <input type="checkbox" className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
-                                                    <div>
-                                                    <img src="https://goccuanho.com/wp-content/uploads/2023/06/Giay-Adidas-Forum-kem-sua-sieu-cap.jpg" alt="" width={200} height={200} />
-                                                        <p className="leading-relaxed">Giày Adidas</p>
-                                                        <p className="leading-relaxed">Quantity: <span className="text-red-500">2</span></p>
-
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Jan 6, 2022</td>
-                                            <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                                <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-emerald-500 bg-emerald-100/60 dark:bg-gray-800">
-                                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M10 3L4.5 8.5L2 6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                    </svg>
-
-                                                    <h2 className="text-sm font-normal">200.000 đ</h2>
-                                                </div>
-                                            </td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                                                <div className="flex items-center gap-x-2">
-                                                    <img className="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
-                                                    <div>
-                                                        <h2 className="text-sm font-medium text-gray-800 dark:text-white ">Quang</h2>
-                                                        <p className="text-xs font-normal text-gray-600 dark:text-gray-400">quang@.com</p>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">đơn hàng đã giao thành công</td>
-                                            <td className='px-4 py-4 text-sm whitespace-nowrap'>
-                                                < select id="cars" className="w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-300">
-                                                <option value="" className="py-2">waiting for delivery</option>
-                                                    <option value="" className="py-2">are delivering</option>
-                                                    <option value="" className="py-2">Successful delivery</option>
-                                                    <option value="" className="py-2">Returns</option>
-                                                </select>
-                                            </td>
-                                            <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                                <div className="flex items-center gap-x-6">
-                                                    <button className="text-gray-500 transition-colors duration-200 dark:hover:text-indigo-500 dark:text-gray-500 hover:text-indigo-500 focus:outline-none">
-                                                        <AiOutlineDelete />
-                                                    </button>
-
-                                                    <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
-                                                        <AiOutlineEdit />
-                                                    </button>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                                                <div className="inline-flex items-center gap-x-3">
-                                                    <input type="checkbox" className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
-                                                    <div>
-                                                    <img src="https://goccuanho.com/wp-content/uploads/2023/06/Giay-Adidas-Forum-kem-sua-sieu-cap.jpg" alt="" width={200} height={200} />
-                                                        <p className="leading-relaxed">Giày Adidas</p>
-                                                        <p className="leading-relaxed">Quantity: <span className="text-red-500">2</span></p>
-
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Jan 6, 2022</td>
-                                            <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                                <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-emerald-500 bg-red-100/60 dark:bg-gray-800">
-                                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M10 3L4.5 8.5L2 6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                    </svg>
-
-                                                    <h2 className="text-sm font-normal">0 đ</h2>
-                                                </div>
-                                            </td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                                                <div className="flex items-center gap-x-2">
-                                                    <img className="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
-                                                    <div>
-                                                        <h2 className="text-sm font-medium text-gray-800 dark:text-white ">Quang</h2>
-                                                        <p className="text-xs font-normal text-gray-600 dark:text-gray-400">quang@.com</p>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">đơn hàng bị hủy</td>
+                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">che tên đơn hàng giúp</td>
                                             <td className='px-4 py-4 text-sm whitespace-nowrap'>
                                                 < select id="cars" className="w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-300">
                                                     <option value="" className="py-2">waiting for delivery</option>
@@ -210,18 +103,11 @@ const index = () => {
                                                     <option value="" className="py-2">Returns</option>
                                                 </select>
                                             </td>
-                                            <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                                <div className="flex items-center gap-x-6">
-                                                    <button className="text-gray-500 transition-colors duration-200 dark:hover:text-indigo-500 dark:text-gray-500 hover:text-indigo-500 focus:outline-none">
-                                                        <AiOutlineDelete />
-                                                    </button>
 
-                                                    <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
-                                                        <AiOutlineEdit />
-                                                    </button>
-                                                </div>
-                                            </td>
                                         </tr>
+
+
+
 
                                     </tbody>
                                 </table>
