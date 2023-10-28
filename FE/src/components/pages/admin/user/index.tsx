a5355c4e94a8dca0d0db8fded67623b5a5924ac8
truongduy03 <truongduy97a@gmail.com>
2023-10-28 21:57:15 +0700

    hiển thị theo role

--- FE/src/components/pages/admin/user/index.tsx
+++ FE/src/components/pages/admin/user/index.tsx
@@ -2,18 +2,21 @@ import { useGetUserQuery } from "@/api/auth";
 import { IUser } from "@/interface/auth";
 import { Button, Table } from "antd";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const AdminUser = () => {
   const { data: userData } = useGetUserQuery();
-  const dataSource = userData?.datas?.map((user: IUser) => ({
-    key: user._id,
-    fullname: user.fullname,
-    username: user.username,
-    phone: user.phone,
-    address: user.address,
-    email: user.email,
-  }));
-console.log(dataSource);
+  const [filterRole, setFilterRole] = useState('all'); // Sử dụng state để theo dõi vai trò được lọc
+
+  // Lọc danh sách người dùng dựa trên vai trò được chọn
+  const filteredUsers = userData?.datas?.filter((user: IUser) => {
+    if (filterRole === 'all') {
+      return true; // Hiển thị tất cả người dùng
+    } else {
+      return user.role === filterRole;
+    }
+  });
+
   const columns = [
     {
       title: "STT",
@@ -22,12 +25,12 @@ console.log(dataSource);
       render: (_text: unknown, _record: unknown, rowIndex: number) => rowIndex + 1,
     },
     {
-      title: "Họ và tên",
+      title: "fullname",
       dataIndex: "fullname",
       key: "fullname",
     },
     {
-      title: "Tên tài khoản",
+      title: "username",
       dataIndex: "username",
       key: "username",
     },
@@ -37,12 +40,12 @@ console.log(dataSource);
       key: "email",
     },
     {
-      title: "Số điện thoại",
+      title: "Phone",
       dataIndex: "phone",
       key: "phone",
     },
     {
-      title: "Địa chỉ",
+      title: "Address",
       dataIndex: "address",
       key: "address",
     },
@@ -50,14 +53,23 @@ console.log(dataSource);
 
   return (
     <div>
-      <header className="flex items-center justify-between mb-4">
-        <h2 className="text-2xl mt-2 ml-2">Quản lý người dùng</h2>
+    <header className="flex items-center justify-between mb-4">
+      <h2 className="text-2xl mt-2 ml-2">Quản lý user</h2>
+      <div className="flex items-center">
+        <select className="p-2 " onChange={(e) => setFilterRole(e.target.value)} style={{ margin: '0 auto',}} >
+          <option value="all">Tất cả</option>
+          <option value="admin">Admin</option>
+          <option value="member">Member</option>
+        </select>
+      </div>
+      <div className="flex items-center">
         <Button className="m-2 mt-4" type="primary" style={{ backgroundColor: "var(--primary-color)", color: "1890ff", border: "none" }}>
           <Link to="/admin/add">Thêm admin</Link>
         </Button>
-      </header>
-      <Table dataSource={dataSource} columns={columns} />;
-    </div>
+      </div>
+    </header>
+    <Table dataSource={filteredUsers} columns={columns} />
+  </div>
   );
 };
 
