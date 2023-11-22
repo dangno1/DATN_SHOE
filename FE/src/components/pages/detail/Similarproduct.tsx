dd1149b82b42cb9d4abad1f43421a5eaecf8be3d
linhnguyen <linhnguyen13092003@gmail.com>
2023-11-22 13:53:41 +0700

    update2

--- FE/src/components/pages/detail/Similarproduct.tsx
+++ FE/src/components/pages/detail/Similarproduct.tsx
@@ -4,12 +4,12 @@ import { IProduct } from "@/interface/product";
 import { BsBagPlus } from "react-icons/bs";
 import { Link } from "react-router-dom";
 const Similarproduct = () => {
-  const { data } = useGetProductsQuery<{data:IProduct[]}>(false);
+  const { data } = useGetProductsQuery<{ data: IProduct[] }>(false);
   console.log(data);
   // const [showTrashCan, setShowTrashCan] = useState(false)
   // const { data: dataProduct } = useGetProductsQuery(showTrashCan);
   // console.log(dataProduct);
-  
+
   const [Similar, setSimilar] = useState<IProduct[]>([]);
   useEffect(() => {
     if (data) {
@@ -25,46 +25,49 @@ const Similarproduct = () => {
   }, [data]);
   return (
     <div>
-     <div className=" 2xl:container 2xl:mx-auto md:py-12 lg:px-20 md:px-6 py-9 px-4">
-      <div className="flex items-center justify-between r">
-        <div className="items-center text-cente gap-12">
-          <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl uppercase ">
-            Sản phẩm nổi bật
-          </h2>
-        </div>
-        <Link to="/products"
-          className="inline-block rounded-lg border bg-white px-4 py-2 uppercase text-center text-sm font-semibold  outline-none ring-indigo-300 transition duration-100 hover:text-white hover:bg-black focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base"
-        >
-          xem thêm
-        </Link>
-      </div>
-      <div className=" grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:grap-8 md:gap-6 gap-4 mt-10">
-        {Similar?.map((product:IProduct)=>(
-            <div className="relative group">
-            <img
-              src={product.image}
-              alt=""
-              className=" lg:block  w-[700px] h-[400px] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-            />
-            <div className=" flex justify-center items-center" />
-            <div className="px-4 py-3 w-72 ">
-              <span className="text-gray-400 mr-3 uppercase text-xs">{product.brand}</span>
-              <p className="text-lg font-bold text-black truncate block capitalize hover:underline">
-               {product.name}
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                 ${product.variants[0].price}
-                </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-              </div>
-            </div>
+      <div className=" 2xl:container 2xl:mx-auto md:py-12 lg:px-20 md:px-6 py-9 px-4">
+        <div className="flex items-center justify-between r">
+          <div className="items-center text-cente gap-12">
+            <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl uppercase ">
+              Sản phẩm khác
+            </h2>
           </div>
-        ))}
+        </div>
+        <section
+          id="Projects"
+          className="w-fit mx-auto grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-14"
+        >
+          {Similar?.map((product: IProduct) => (
+            <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" key={product.id}>
+              <Link to={`/detail/${product._id}`}>
+                <img
+                  src={product?.image}
+                  alt="Product"
+                  className="h-80 w-72 object-cover rounded-t-xl"
+                />
+                <div className="px-4 py-3 w-72">
+                  <span className="text-gray-400 mr-3 uppercase text-xs">
+                    {product?.brand}
+                  </span>
+                  <p className="text-lg font-bold text-black truncate block capitalize hover:underline">
+                    {product?.name}
+                  </p>
+                  <div className="flex items-center">
+                    <p className="text-lg font-semibold cursor-auto my-3 text-red-500">
+                      {product?.variants[0].discount?.toLocaleString('vi-VN')} VND
+                    </p>
+                    <del>
+                      <p className="text-sm text-gray-600 cursor-auto ml-2">
+                        {product?.variants[0].price?.toLocaleString('vi-VN')} VND
+                      </p>
+                    </del>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </section>
       </div>
-    </div>
     </div>
   );
 };
